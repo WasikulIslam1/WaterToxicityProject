c3229962ac6161639e8f204a008944bcd246dc7b
WasikulIslam1 <wasikul.islam@baruchmail.cuny.edu>
2024-04-28 16:53:12 -0400

    updated etl and postgres

--- datamodel/watertoxicity_dw.sql
+++ datamodel/watertoxicity_dw.sql
@@ -2,8 +2,13 @@ CREATE SCHEMA INSTANCE;
 
 CREATE TABLE watertoxicity_lgl.INSTANCE.dim_analyte ( 
 	analyte_id int64 NOT NULL  ,
-	analyte_name string  ,
-	dw_analyte_name string  
+	dw_analyte_name string  ,
+	analytedescr string  ,
+	group1 string  ,
+	group2 string  ,
+	group3 string  ,
+	group4 string  ,
+	analytedate date  
  );
 
 ALTER TABLE watertoxicity_lgl.INSTANCE.dim_analyte ADD PRIMARY KEY ( analyte_id )  NOT ENFORCED;
@@ -11,8 +16,7 @@ ALTER TABLE watertoxicity_lgl.INSTANCE.dim_analyte ADD PRIMARY KEY ( analyte_id 
 CREATE TABLE watertoxicity_lgl.INSTANCE.dim_collection ( 
 	collection_id int64 NOT NULL  ,
 	collection_time string  ,
-	collectiondepth numeric  ,
-	collectionmethod string  ,
+	collectiondepth numeric(10,5)  ,
 	collectiondevice string  ,
 	positionwatercolumn string  
  );
@@ -21,7 +25,6 @@ ALTER TABLE watertoxicity_lgl.INSTANCE.dim_collection ADD PRIMARY KEY ( collecti
 
 CREATE TABLE watertoxicity_lgl.INSTANCE.dim_date ( 
 	date_id int64 NOT NULL  ,
-	date date  ,
 	year int64  ,
 	quarter int64  ,
 	month int64  ,
@@ -44,25 +47,26 @@ ALTER TABLE watertoxicity_lgl.INSTANCE.dim_sample ADD PRIMARY KEY ( sample_id ) 
 
 CREATE TABLE watertoxicity_lgl.INSTANCE.dim_station ( 
 	station_id int64 NOT NULL  ,
-	stationcode string  ,
 	stationname string  ,
-	latitude numeric  ,
-	longitude numeric  ,
+	latitude numeric(10,5)  ,
+	longitude numeric(10,5)  ,
 	datum string  
  );
 
 ALTER TABLE watertoxicity_lgl.INSTANCE.dim_station ADD PRIMARY KEY ( station_id )  NOT ENFORCED;
 
 CREATE TABLE watertoxicity_lgl.INSTANCE.facts_waterquality ( 
+	sample_id int64 NOT NULL  ,
 	fact_id int64 NOT NULL  ,
 	station_id int64 NOT NULL  ,
-	date_id int64 NOT NULL  ,
 	analyte_id int64 NOT NULL  ,
-	collectiondepth numeric  ,
-	result numeric  ,
-	calculatedvalue numeric  ,
-	sample_id int64 NOT NULL  ,
-	collection_id int64 NOT NULL  
+	collection_id int64 NOT NULL  ,
+	date_id int64 NOT NULL  ,
+	percenteffect int64 NOT NULL  ,
+	calculatedvalue int64  ,
+	evalthreshold numeric(10,5)  ,
+	dilution numeric(10,5)  ,
+	result string  
  );
 
-ALTER TABLE watertoxicity_lgl.INSTANCE.facts_waterquality ADD PRIMARY KEY ( fact_id )  NOT ENFORCED;
+ALTER TABLE watertoxicity_lgl.INSTANCE.facts_waterquality ADD PRIMARY KEY ( fact_id, date_id, collection_id, station_id, sample_id, analyte_id )  NOT ENFORCED;
